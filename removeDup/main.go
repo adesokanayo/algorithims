a4a26d850d644ec11ab86fc6bb78423d7e38386b
Ayo Adesokan <adesokanayo@gmail.com>
2021-04-10 10:08:27 -0600

    Added duplicate removal

--- removeDup/main.go
+++ removeDup/main.go
@@ -1,23 +1,42 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+
+	var arr1 []int
+	var arr2 []string
+	arr1 = append(arr1, 0, 1, 2, 3, 4, 5, 5, 6, 6, 7)
+	arr2 = append(arr2, "a", "b", "b", "c", "d", "e", "f")
+	fmt.Println(removeDupInt(arr1))
+
+	fmt.Println(removeDupString(arr2))
 
 }
 
-func removeDup(a []string, b []string) (c []string) {
+// removeDupInt is based on a two pointer method.
+func removeDupInt(arr []int) (result []int) {
 
-	slice := make([]int, 0)
-	checker := make(map[int]int)
-
-	d := append(a, b...)
-	result := make([]int, 0)
-
-	for _, val := range d {
-		slice[val] = 1
+	slow := 0
+	for fast := 0; fast < len(arr); fast++ {
+		if arr[fast] != arr[slow] {
+			slow++
+			arr[slow] = arr[fast]
+		}
 	}
+	return arr[:slow+1]
+}
 
-	for ful := range slice{
-		append(result, ful)
+func removeDupString(arr []string) (result []string) {
+
+	slow := 0
+	for fast := 0; fast < len(arr)-1; fast++ {
+		if arr[fast] != arr[slow] {
+			slow++
+			arr[slow] = arr[fast]
+		}
 	}
-	return result
+	return arr[0 : slow+1]
 }
